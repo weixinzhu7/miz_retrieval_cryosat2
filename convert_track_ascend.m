463ae047e39950b12c753f426c3e9f566b4f463b
weixinzhu97 <57586019+weixinzhu97@users.noreply.github.com>
2023-07-18 16:08:02 +0800

    Update convert_track_ascend.m

--- convert_track_ascend.m
+++ convert_track_ascend.m
@@ -1,6 +1,6 @@
 function [cs2_baselined_track_new]=convert_track_ascend(cs2_baselined_track_data)
 % input:alongtrack cs2 parameter 
-% output: All data is arranged in ascending track direction and excludes the footprint of land.
+% output: All data is arranged in ascending track direction.
 %
 % Author Info
 %   This function and supporting documentation were written by Weixin Zhu
@@ -14,13 +14,5 @@ if(min_latitude~=1)
 else
     cs2_baselined_track_new=cs2_baselined_track_data;  clear cs2_baselined_track_data;
 end
-
-cs2_island_flag=landmask(cs2_baselined_track_new(:,2),cs2_baselined_track_new(:,1));
-cs2_island_flag_double=double(cs2_island_flag);
-cs2_island_flag_index1=find(cs2_island_flag_double==1);
-cs2_island_flag_index2=max(cs2_island_flag_index1):1:max(cs2_island_flag_index1)+50;
-cs2_island_flag_index2=cs2_island_flag_index2';
-cs2_island_flag_index=[cs2_island_flag_index1;cs2_island_flag_index2];
-cs2_baselined_track_new(cs2_island_flag_index,:)=[];
 
 end
